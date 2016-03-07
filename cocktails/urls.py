e7210d4f52f97d31ca083f62a9cc69bca3125418
Artem <pbi62007@yandex.ru>
2016-03-07 22:06:21 +0300

    Add the namespace for 'recipes_adviser' app.

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^recipes/', include('recipes_adviser.urls')),
+    url(r'^recipes/', include('recipes_adviser.urls',
+                              namespace='recipes_adviser')),
 ]
