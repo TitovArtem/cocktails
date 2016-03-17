97f6a6a69277b9e7ac978c71b793879391e40649
Artem <pbi62007@yandex.ru>
2016-03-18 01:09:09 +0300

    close #2 Creating admin interface for filling database

--- recipes_adviser/admin.py
+++ recipes_adviser/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
 
-from .models import Ingredient
+from .models import Ingredient, Recipe, RecipeStage, CocktailComponent
 
 
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'liquid', 'abv')
+    list_display = ('name', 'liquid', 'abv', 'image')
+    search_fields = ('name', )
+
+
+class CocktailComponentAdmin(admin.ModelAdmin):
+    raw_id_fields = ['ingredient']
+
+
+class RecipeStageInline(admin.StackedInline):
+    model = Recipe.stages.through
+    verbose_name = 'Stage'
+    verbose_name_plural = "Recipe stages"
+
+
+class CocktailComponentInline(admin.StackedInline):
+    model = Recipe.ingredients.through
+    verbose_name = 'Component'
+    verbose_name_plural = 'Cocktail components'
+
+
+class RecipeAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,              {'fields': ['title', 'author', 'description',
+                                        'title_image']}),
+    ]
+    inlines = [RecipeStageInline, CocktailComponentInline]
+
 
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeStage)
+admin.site.register(CocktailComponent, CocktailComponentAdmin)
