94ce52b5edecce4e16d7a7c425463fdc0bd70e00
Artem <pbi62007@yandex.ru>
2016-03-21 11:51:32 +0300

    #1 Rename the <name> field in urls for ingredient's detail page.

--- recipes_adviser/urls.py
+++ recipes_adviser/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ingredients/$', views.ingredients, name='ingredients'),
     url(r'^ingredient/(?P<ingredient_id>[0-9]+)/$',
-        views.ingredient, name='detail'),
+        views.ingredient, name='ingredient_detail'),
     url(r'^search/$', views.search, name='search'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^contact/thanks/$', views.thanks, name='thanks'),
