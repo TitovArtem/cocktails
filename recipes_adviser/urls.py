d051ced49a2765ad4dd20c6fba7bebe290958791
Artem <pbi62007@yandex.ru>
2016-05-28 18:50:45 +0300

    closes #13 Add redirect to index page on logo click.

--- recipes_adviser/urls.py
+++ recipes_adviser/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'recipes_adviser'
+
 urlpatterns = [
-    url(r'^$', views.RecipeSearchView.as_view()),
+    url(r'^$', views.RecipeSearchView.as_view(), name='index'),
     url(r'^ingredient/(?P<ingredient_id>[0-9]+)/$',
         views.ingredient, name='ingredient_detail'),
     url(r'^recipe/(?P<recipe_id>[0-9]+)/$', views.recipe, name='recipe_detail'),
